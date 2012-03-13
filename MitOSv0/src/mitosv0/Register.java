ed8988e8df7555fbd7363c4ba834c0b3dad354a6
Tomas <Tomas@Tomas-PC>
2012-03-13 21:07:37 +0200

    Word created

---
+++ MitOSv0/src/mitosv0/Register.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mitosv0;
+
+/**
+ *
+ * @author Tomas
+ */
+public abstract class Register {
+    private byte[] data;
+    
+    public void setValue()
+    {
+            
+    }
+    
+}
