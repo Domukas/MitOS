dd93245b39948624277fd3ff045bdf740c38bd82
Dominykas Kiauleikis <domukas64@gmail.com>
2012-03-16 12:57:34 +0200

    Sujungiu sakas.

--- MitOSv0/src/mitosv0/registers/PLRRegister.java
+++ MitOSv0/src/mitosv0/registers/PLRRegister.java
@@ -20,4 +20,47 @@ public class PLRRegister extends Register {
         return data;
     }
     
+    public byte getA0()
+    {
+        return (byte)(data >>> 6);
+    }
+    
+    public void setA0(byte value)
+    {
+        int intValue = (int) value & 0xff;
+        data = (data & 0x00FFFFFF) | (intValue << 6);
+    }
+    
+    public int getA1()
+    {
+        return (data / 0x10000) % 0x100;
+    }
+    
+    public void setA1(byte value)
+    {
+        int intValue = (int) value & 0xff;
+        data = (data & 0xFF00FFFF) | (intValue << 4);
+    }
+    
+    public int getA2()
+    {
+        return (data / 0x100) % 0x10000;
+    }
+    
+    public void setA2(byte value)
+    {
+        int intValue = (int) value & 0xff;
+        data = (data & 0xFFFF00FF) | (intValue << 2);
+    }
+    
+    public int getA3()
+    {
+        return data % 0x1000000;
+    }
+    
+    public void setA3(byte value)
+    {
+        int intValue = (int) value & 0xff;
+        data = (data & 0xFFFFFF00) | intValue;
+    }
 }
