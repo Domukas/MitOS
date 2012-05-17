321c2ea8730cc462b36cdea95fc006aeff45df40
Zorian Ivanec <zorian.ivanec@gmail.com>
2012-05-17 14:05:32 +0300

    ResourceManager

--- tdzVmRm/src/tdzOS/ResourceManager.java
+++ tdzVmRm/src/tdzOS/ResourceManager.java
@@ -4,6 +4,8 @@
  */
 package tdzOS;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author Tomas
@@ -16,65 +18,84 @@ class ResourceManager {
     {
         this.os = os;
     }
-    
-    //paskirstytojas konkreciam procesui, duodant resursus, kurie skaidomi, pvz atminits.
+    //---------------------------------------------------------
+    //paskirstytojas konkreciam procesui
     public void Execute(Process process, Resource r, int count)
-    {
-        //TODO
-        
-        //Prašydamas resurso ar norėdamas jį atlaisvinti,
-        //procesas kreipiasi į atitinkamą resursų paskirstytoją, kuris privalo jį aptarnauti.
-        //Resursų paskirstytojo paskirtis – suteikti paprašytą resurso elementų kiekį procesui.
-        
-        //turi būti numatyta galimybė atiduoti resurso elementą konkrečiam procesui,
-        //arba prašyti kelių resurso elementų. Resurso paskirstytojas peržvelgia visus laukiančius
-        //šio resurso procesų sąrašą, ir, sutikęs galimybę aptarnauti procesą,
-        //perduoda jam reikalingus resurso elementus ir pažymi jį pasiruošusiu.
-        
-        os.processManager.Execute();
-    }
-    
-    //paskirstytojas konkreciam procesui, duodant resursus kurie yra neskaidomi i komponentus
-    public void Execute(Process process, Resource r)
-    {
-        //TODO
-
-        os.processManager.Execute();
-    }
-    
-    //paskirstytojas visiem laukiantiems resursams kai resursas skaidomas dalimis
-    public void Execute(Resource r, int count)
-    {
+    {     
         //jei yra procesu kuriems res reikalingas
-        if(r.rd.waitingProcesses.size() != 0)
+        if (r.rd.waitingProcesses.size() != 0)
         {
-            //tada ziurim ar toks resursas galimas atiduoti
-           // r.rd.externalIDas
-        }
-        
+            //duodam resursa arba jo dali procesui
+            giveToProcessAResourceComponents(process, r, count);
+            //jei tam procesui daugiau jokiu resursu nereikia, pazymim ji pasiruosusiu
+            readyTheProcess(process);
+        } 
+        //kvieciam procesu planuotoja
         os.processManager.Execute();
     }
-    
-    //paskirstytojas visiems laukiantiems resursams kai resursas neskaidomas
-    public void Execute(Resource r)
+    //----------------------------------------
+    //paskirstytojas visiems procesams
+    public void Execute(Resource r, int count)
     {
         //jei yra procesu kuriems res reikalingas
         if (r.rd.waitingProcesses.size() != 0)
         {
-            int priority = 0;
-            Process tmpProcess = null;
             //tai randam su didziausiu prioritetu procesa
-            for(int i = 0; i < r.rd.waitingProcesses.size(); i++)
-            {
-                if(priority < r.rd.waitingProcesses.get(i).pd.priority)
-                {
-                    tmpProcess = r.rd.waitingProcesses.get(i);
-                }
-            }
-            //jam duodam resurso nuoroda ir pasalinam resursa is laisvu res saraso.
-            tmpProcess.pd.ownedResources.add(r);
-            os.resources.remove(r);
+            Process tmpProcess = findHighestProcessWhereResourceIsNeeded(r);
+            //duodam resursa arba jo dali procesui
+            giveToProcessAResourceComponents(tmpProcess, r, count);
             //jei tam procesui daugiau jokiu resursu nereikia, pazymim ji pasiruosusiu
+            readyTheProcess(tmpProcess);
+        } 
+        //kvieciam procesu planuotoja
+        os.processManager.Execute();
+    }
+    //---------------------------------------------------------
+    Process findHighestProcessWhereResourceIsNeeded(Resource r)
+    {
+        int priority = 0;
+        Process tmpProcess = null;
+        for(int i = 0; i < r.rd.waitingProcesses.size(); i++)
+        {
+            if(priority < r.rd.waitingProcesses.get(i).pd.priority)
+            {
+                priority = r.rd.waitingProcesses.get(i).pd.priority;
+                tmpProcess = r.rd.waitingProcesses.get(i);
+            }
+        }
+        return tmpProcess;
+    }
+    //-----------------------------------------------------------------------------
+    void giveToProcessAResourceComponents(Process tmpProcess, Resource r, int count)
+    {
+        if((r.rd.components.size() >= count) && (tmpProcess != null))
+            {
+                //sukuriam tuscia komponentu sarasa
+                LinkedList<Object> newComponents = new LinkedList<Object>();
+                //tuomet priskiriam tam naujam resursui tuos komponentus, o is originalo juos isimam
+                for(int i = 0; i < count; i++)
+                {
+                    Object tmpComponent = r.rd.components.get(i);
+                    r.rd.components.remove(i);
+                    newComponents.add(tmpComponent);
+                }
+                //sukuriam resursa identiska originalui bet su kitu componentu sarasu
+                Resource tmpR = new Resource(r.rd.creator, r.rd.externalID, r.rd.internalID, r.rd.reusable, newComponents, r.rd.waitingProcesses, r.rd.resourceManager);
+                //atiduodam ta nauja resursa procesui
+                tmpProcess.pd.ownedResources.add(tmpR);
+                
+                //jei resursas nepanaudojamas dar karta tai ismetam ji is saraso
+                if(!r.rd.reusable)
+                {
+                    os.resources.remove(r);
+                }
+            } 
+    }
+    //--------------------------------------
+    void readyTheProcess(Process tmpProcess)
+    {
+        if(tmpProcess != null)
+        {
             boolean stillNeeded = false;
             for(int i = 0; i < os.resources.size(); i++)
             {
@@ -90,8 +111,7 @@ class ResourceManager {
             {
                 tmpProcess.pd.state =  OS.ProcessState.Ready;
             }
-        } 
-        //kvieciam procesu planuotoja
-        os.processManager.Execute();
+        }
     }
+    //---------------------    
 }
